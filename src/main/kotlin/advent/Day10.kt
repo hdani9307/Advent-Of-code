38ba032e7ddc9abb8c7d10ca1330980343548026
Daniel Hoffmann <hoffmann.daniel.1993@gmail.com>
2022-12-16 21:11:21 +0100

    Day10_PT2

--- src/main/kotlin/advent/Day10.kt
+++ src/main/kotlin/advent/Day10.kt
@@ -6,6 +6,7 @@ import java.io.IOException
 
 class Day10 {
     fun run() {
+        val matrix = mutableListOf<String>()
         var queue = mutableListOf<String>()
         BufferedReader(FileReader("/Users/hoffmann-daniel-mbp/dev/advent/advent/src/main/resources/day10.txt")).use {
             try {
@@ -22,26 +23,58 @@ class Day10 {
                 e.printStackTrace()
             }
         }
-        var index = 1
-        var x = 1
-        val signals = mutableListOf<Int>()
+        var index = 0
+
+        var pixLeft = 0
+        var pix0 = 1
+        var pixRight = 2
+
+        //  If the sprite is positioned such that one of its three pixels is the pixel currently being drawn,
+        //  the screen produces a lit pixel (#); otherwise,
+        //  the screen leaves the pixel dark (.).
         for (operation in queue.iterator()) {
-            signals.add(x * index)
-            if (operation != "noop") {
-                x += operation.split(" ")[1].toInt()
+
+            if (operation == "noop") {
+                //begin executing addx 15 or noop
             }
+
+            //CRT draws pixel in position
+            if (index == pixLeft || index == pix0 || index == pixRight) {
+                matrix.add("#")
+            } else {
+                matrix.add(".")
+            }
+
+            if (operation != "noop") {
+                pix0 += operation.split(" ")[1].toInt()
+                pixLeft = pix0 - 1
+                pixRight = pix0 + 1
+            }
+
             index++
+            if(index % 40 == 0){
+                index = 0
+            }
         }
-        //14840
-        //14760
-        println(signals)
-        println(signals[19])
-        println(signals[59])
-        println(signals[99])
-        println(signals[139])
-        println(signals[179])
-        println(signals[219])
-        println(signals[19] + signals[59] + signals[99] + signals[139] + signals[179] + signals[219])
-        //13140
+
+        for (x in 0 until 40 * 6) {
+            print(matrix[x])
+            if ((x + 1) % 40 == 0) {
+                println()
+            }
+        }
+        //##..##..##..##..##..##..##..##..##..##..
+        //###...###...###...###...###...###...###.
+        //####....####....####....####....####....
+        //#####.....#####.....#####.....#####.....
+        //######......######......######......####
+        //#######.......#######.......#######.....
+
+        //##..##..##..##..##..##..##..##..##..##..
+        //###...###...###...###...###...###...###.
+        //####....####....####....####....####....
+        //#####.....#####.....#####.....#####.....
+        //######......######......######......####
+        //#######.......#######.......#######.....
     }
 }
