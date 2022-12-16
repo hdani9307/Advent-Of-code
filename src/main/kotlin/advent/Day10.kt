e9d0b6e75e362442a0cb60e565d5bf1153d993b3
Daniel Hoffmann <hoffmann.daniel.1993@gmail.com>
2022-12-16 19:09:02 +0100

    Day9_PT2

---
+++ src/main/kotlin/advent/Day10.kt
@@ -0,0 +1,4 @@
+package advent
+
+class Day10 {
+}
--- src/main/kotlin/advent/Day9.kt
+++ src/main/kotlin/advent/Day9.kt
@@ -4,15 +4,19 @@ import java.io.BufferedReader
 import java.io.FileReader
 import java.io.IOException
 import kotlin.math.abs
+import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 class Day9 {
     fun run() {
 
-        var head = Pair(0, 0)
-        var tail = Pair(0, 0)
-        var previousHead: Pair<Int, Int>
 
-        val visited = mutableSetOf(tail)
+        val rope = mutableListOf<Pair<Int, Int>>()
+        for (i in 0 until 10) {
+            rope.add(Pair(0, 0))
+        }
+
+        val visited = mutableSetOf(rope[0])
 
         BufferedReader(FileReader("/Users/hoffmann-daniel-mbp/dev/advent/advent/src/main/resources/day9.txt")).use {
             try {
@@ -21,69 +25,39 @@ class Day9 {
                     val split = line.split(" ");
                     val direction = split[0]
                     val steps = split[1].toInt()
-
-                    if (direction == "R") {
-                        for (step in 0 until steps) {
-
-                            previousHead = head
+                    for (step in 0 until steps) {
+                        var head = rope[0]
+                        if (direction == "R") {
                             head = Pair(head.first, head.second + 1)
-
-                            if (abs(head.first - tail.first) >= 2) {
-                                tail = previousHead
-                                visited.add(tail)
-                            }
-                            if (abs(head.second - tail.second) >= 2) {
-                                tail = previousHead
-                                visited.add(tail)
-                            }
                         }
-                    }
-                    if (direction == "L") {
-                        for (step in 0 until steps) {
-                            previousHead = head
+                        if (direction == "L") {
                             head = Pair(head.first, head.second - 1)
-
-                            if (abs(head.first - tail.first) >= 2) {
-                                tail = previousHead
-                                visited.add(tail)
-                            }
-                            if (abs(head.second - tail.second) >= 2) {
-                                tail = previousHead
-                                visited.add(tail)
-                            }
                         }
-                    }
-                    if (direction == "U") {
-                        for (step in 0 until steps) {
-                            previousHead = head
+                        if (direction == "U") {
                             head = Pair(head.first - 1, head.second)
-
-                            if (abs(head.first - tail.first) >= 2) {
-                                tail = previousHead
-                                visited.add(tail)
-                            }
-                            if (abs(head.second - tail.second) >= 2) {
-                                tail = previousHead
-                                visited.add(tail)
-                            }
-
                         }
-                    }
-                    if (direction == "D") {
-                        for (step in 0 until steps) {
-                            previousHead = head
+                        if (direction == "D") {
                             head = Pair(head.first + 1, head.second)
+                        }
+                        rope[0] = head
 
-                            if (abs(head.first - tail.first) >= 2) {
-                                tail = previousHead
-                                visited.add(tail)
-                            }
-                            if (abs(head.second - tail.second) >= 2) {
-                                tail = previousHead
-                                visited.add(tail)
+                        for (knot in rope.subList(0, rope.size - 1).withIndex()) {
+                            val diff = Pair(
+                                rope[knot.index].first - rope[knot.index + 1].first,
+                                rope[knot.index].second - rope[knot.index + 1].second,
+                            );
+
+                            if (diff.first.absoluteValue > 1 || diff.second.absoluteValue > 1) {
+                                val pair = Pair(
+                                    rope[knot.index + 1].first + diff.first.sign,
+                                    rope[knot.index + 1].second + diff.second.sign
+                                )
+                                rope[knot.index + 1] = pair
+                                if (knot.index + 1 == 9) {
+                                    visited.add(rope[knot.index + 1])
+                                }
                             }
                         }
-
                     }
 
                     line = it.readLine()
