acdebbdaaeec54beba96705fb819f90a8232f70c
Hoffmann Daniel <hoffmann.daniel@gbsolutions.io>
2023-12-06 12:34:03 +0100

    Day5 Pt2

--- src/main/kotlin/advent/Main.java
+++ src/main/kotlin/advent/Main.java
@@ -4,7 +4,16 @@ package advent;
 import advent._2023.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        new Day5Part1().run();
+    public static void main(String[] args) {
+        var now = System.currentTimeMillis();
+        try {
+
+            new Day5Part2().run();
+        } catch (Exception e) {
+
+        } finally {
+            var end = System.currentTimeMillis() - now;
+            System.out.println("Run sec " + end / 1000d);
+        }
     }
 }
--- src/main/kotlin/advent/_2023/Day5.kt
+++ src/main/kotlin/advent/_2023/Day5.kt
@@ -1,6 +1,13 @@
 package advent._2023
 
 import advent.Day
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Objects
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicLong
+
 
 class Day5Part1 : Day("5") {
 
@@ -8,13 +15,13 @@ class Day5Part1 : Day("5") {
         val seeds = lines[0].replace("seeds: ".toRegex(), "")
             .split(" ")
             .map { it.toLong() }
-        val seedToSoil = mutableMapOf<Long, Long?>()
-        val soilToFertilizer = mutableMapOf<Long, Long?>()
-        val fertilizerToWater = mutableMapOf<Long, Long?>()
-        val waterToLight = mutableMapOf<Long, Long?>()
-        val lightToTemperature = mutableMapOf<Long, Long?>()
-        val temperatureToHumidity = mutableMapOf<Long, Long?>()
-        val humidityToLocation = mutableMapOf<Long, Long?>()
+        val seedToSoil = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val soilToFertilizer = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val fertilizerToWater = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val waterToLight = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val lightToTemperature = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val temperatureToHumidity = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val humidityToLocation = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
 
         val mapList = listOf(
             seedToSoil,
@@ -26,7 +33,7 @@ class Day5Part1 : Day("5") {
             humidityToLocation
         )
         val iterator = mapList.listIterator()
-        var currentMap: MutableMap<Long, Long?>? = null;
+        var currentMap: MutableMap<Pair<Long, Long>, Pair<Long, Long>?>? = null;
         for (line in lines) {
             if (line.startsWith("seeds")) {
                 continue
@@ -36,10 +43,11 @@ class Day5Part1 : Day("5") {
                     val split = line.split(" ").map { it.toLong() }
                     val destinationRangeStart = split[0]
                     val sourceRangeStart = split[1]
-                    val rangeLength = split[2]
-                    for (i in 0 until rangeLength) {
-                        currentMap?.set(sourceRangeStart + i, destinationRangeStart + i)
-                    }
+                    val rangeLength = split[2] - 1
+                    currentMap?.set(
+                        Pair(sourceRangeStart, sourceRangeStart + rangeLength),
+                        Pair(destinationRangeStart, destinationRangeStart + rangeLength)
+                    )
                 } else {
                     if (iterator.hasNext()) {
                         currentMap = iterator.next()
@@ -49,21 +57,140 @@ class Day5Part1 : Day("5") {
         }
         var minLocation = Long.MAX_VALUE
         for (seed in seeds) {
-            val location = asd(seed, mapList.listIterator())
+            val location = searchHeight(seed, mapList.listIterator())
             minLocation = minLocation.coerceAtMost(location);
         }
+        // 662197086
         println(minLocation)
     }
 
-    private fun asd(source: Long, searchMapIterator: Iterator<MutableMap<Long, Long?>>): Long {
+    private fun searchHeight(
+        source: Long,
+        searchMapIterator: ListIterator<MutableMap<Pair<Long, Long>, Pair<Long, Long>?>>
+    ): Long {
         if (!searchMapIterator.hasNext()) {
             // TODO
             return source;
         }
         val currentSearchMap = searchMapIterator.next()
-        if (!currentSearchMap.containsKey(source)) {
-            currentSearchMap[source] = source
+
+        // containsKey
+        var startRange: Pair<Long, Long>? = null;
+        var destinationRange: Pair<Long, Long>? = null;
+
+        for (entry in currentSearchMap.entries) {
+            if (entry.key.first <= source && source <= entry.key.second) {
+                startRange = entry.key
+                destinationRange = entry.value
+                break
+            }
         }
-        return asd(currentSearchMap[source]!!, searchMapIterator)
+        if (startRange == null) {
+            startRange = Pair(source, source)
+            destinationRange = Pair(source, source)
+        }
+        val diff = source - startRange.first
+        val newSource = destinationRange!!.first + diff
+        return searchHeight(newSource, searchMapIterator)
+    }
+}
+
+class Day5Part2 : Day("5") {
+
+    val lock = Object()
+
+    override fun run() {
+        val tmp = lines[0].replace("seeds: ".toRegex(), "")
+            .split(" ")
+            .map { it.toLong() }
+
+        val seeds = mutableListOf<Pair<Long, Long>>()
+        for (i in 1..tmp.size step 2) {
+            seeds.add(Pair(tmp[i - 1], tmp[i - 1] + tmp[i]))
+        }
+        val seedToSoil = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val soilToFertilizer = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val fertilizerToWater = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val waterToLight = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val lightToTemperature = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val temperatureToHumidity = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+        val humidityToLocation = mutableMapOf<Pair<Long, Long>, Pair<Long, Long>?>()
+
+        val mapList = listOf(
+            seedToSoil,
+            soilToFertilizer,
+            fertilizerToWater,
+            waterToLight,
+            lightToTemperature,
+            temperatureToHumidity,
+            humidityToLocation
+        )
+        val iterator = mapList.listIterator()
+        var currentMap: MutableMap<Pair<Long, Long>, Pair<Long, Long>?>? = null;
+        for (line in lines) {
+            if (line.startsWith("seeds")) {
+                continue
+            }
+            if (line.isNotBlank()) {
+                if (line.matches("^[0-9].*".toRegex())) {
+                    val split = line.split(" ").map { it.toLong() }
+                    val destinationRangeStart = split[0]
+                    val sourceRangeStart = split[1]
+                    val rangeLength = split[2] - 1
+                    currentMap?.set(
+                        Pair(sourceRangeStart, sourceRangeStart + rangeLength),
+                        Pair(destinationRangeStart, destinationRangeStart + rangeLength)
+                    )
+                } else {
+                    if (iterator.hasNext()) {
+                        currentMap = iterator.next()
+                    }
+                }
+            }
+        }
+        var minLocation = Long.MAX_VALUE
+        for ((index, seed) in seeds.withIndex()) {
+            println("Processing seed $index")
+            for (i in seed.first until seed.second) {
+                val location = searchHeight(i, mapList.listIterator())
+                minLocation = minLocation.coerceAtMost(location);
+            }
+            println("$index finished, currentMin: $minLocation")
+        }
+
+        //52510810 - high
+        //52510809 - ok
+        println(minLocation)
+
+    }
+
+    private fun searchHeight(
+        source: Long,
+        searchMapIterator: ListIterator<MutableMap<Pair<Long, Long>, Pair<Long, Long>?>>
+    ): Long {
+        if (!searchMapIterator.hasNext()) {
+            // TODO
+            return source;
+        }
+        val currentSearchMap = searchMapIterator.next()
+
+        // containsKey
+        var startRange: Pair<Long, Long>? = null;
+        var destinationRange: Pair<Long, Long>? = null;
+
+        for (entry in currentSearchMap.entries) {
+            if (entry.key.first <= source && source <= entry.key.second) {
+                startRange = entry.key
+                destinationRange = entry.value
+                break
+            }
+        }
+        if (startRange == null) {
+            startRange = Pair(source, source)
+            destinationRange = Pair(source, source)
+        }
+        val diff = source - startRange.first
+        val newSource = destinationRange!!.first + diff
+        return searchHeight(newSource, searchMapIterator)
     }
 }
