d558c65096b6fd1cf09dcc0ac42adf6e464c230d
daniel.hoffmann <hoffmann.daniel.1993@gmail.com>
2024-12-05 09:16:20 +0100

    WIP Day5 P2

---
+++ src/test/kotlin/advent/_2024/day5/Day5Part2.kt
@@ -0,0 +1,77 @@
+package advent._2024.day5
+
+import advent.helper.readInput
+import advent.helper.runMeasured
+import org.junit.jupiter.api.Assertions
+import kotlin.test.Test
+
+class Day5Part2 {
+
+    @Test
+    fun run() {
+        val lines = readInput("2024/sample.txt")
+        runMeasured {
+            val notCorrects = mutableListOf<String>()
+            val x = mutableMapOf<String, MutableList<String>>()
+            val y = mutableMapOf<String, MutableList<String>>()
+            for (line in lines) {
+                if (line.contains("|")) {
+                    val split = line.split("|")
+                    val key = split[0].trim()
+                    val value = split[1].trim()
+                    x.putIfAbsent(key, mutableListOf())
+                    x[key]?.add(value)
+
+                    y.putIfAbsent(value, mutableListOf())
+                    y[value]?.add(key)
+                } else if (line.isEmpty()) {
+                    continue
+                } else {
+                    val split = line.split(",")
+                    var correct = true
+                    for (i in split.withIndex()) {
+                        val value = i.value.trim()
+                        val children = x[value]
+                        val parents = y[value]
+                        for (j in split.withIndex()) {
+                            if (j.index < i.index) {
+                                if (parents == null) {
+                                    continue
+                                }
+                                if (!parents.contains(j.value)) {
+                                    correct = false
+                                }
+                            }
+                            if (j.index > i.index) {
+                                if (children == null) {
+                                    continue
+                                }
+                                if (!children.contains(j.value)) {
+                                    correct = false
+                                }
+                            }
+
+                        }
+                    }
+                    if (!correct) {
+                        notCorrects.add(line)
+                    }
+                }
+            }
+            val ordered = mutableListOf<String>()
+            for (notCorrect in notCorrects) {
+                val split = notCorrect.split(",").map { it.toInt() }.sorted()
+                ordered.add(split.joinToString(","))
+            }
+
+            var sum = 0
+            for (c in ordered) {
+                val split = c.split(",")
+                val middle = split[(split.size - 1) / 2]
+                sum += middle.toInt()
+            }
+            println(sum)
+            Assertions.assertEquals(4578, sum)
+        }
+    }
+}
