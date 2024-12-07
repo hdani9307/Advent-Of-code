d776e4690f9e6f9299c1ec6712e315bb4c80dd57
daniel.hoffmann <hoffmann.daniel.1993@gmail.com>
2024-12-07 13:53:33 +0100

    Day7

---
+++ src/test/kotlin/advent/_2024/day7/Day7Part1.kt
@@ -0,0 +1,80 @@
+package advent._2024.day7
+
+import advent.helper.readInput
+import advent.helper.runMeasured
+import org.junit.jupiter.api.Assertions
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.test.Test
+
+data class TreeNode(
+    val value: Long,
+    val children: MutableList<TreeNode> = mutableListOf()
+)
+
+class Day7Part1 {
+
+    @Test
+    fun run() {
+        val lines = readInput("2024/7.txt")
+        var sum = 0L
+
+        runMeasured {
+            for (line in lines) {
+                val split = line.split(":")
+                val target = split[0].toLong()
+
+                val values = split[1].split(" ").toMutableList()
+                values.removeIf { it.isEmpty() }
+                var tree = buildTree(values[0].toLong() + values[1].toLong(), values)
+
+                val result = AtomicBoolean(false)
+                findLastNode(tree, target, result)
+                if (!result.get()) {
+                    tree = buildTree(values[0].toLong() * values[1].toLong(), values)
+                    findLastNode(tree, target, result)
+                }
+
+                if (result.get()) {
+                    sum += target
+                }
+            }
+            println(sum)
+            Assertions.assertEquals(7579994664753, sum)
+        }
+    }
+
+    private fun findLastNode(node: TreeNode, target: Long, accumulator: AtomicBoolean) {
+        if (node.children.isEmpty() && node.value == target) {
+            accumulator.set(true)
+            return
+        }
+        for (child in node.children) {
+            findLastNode(child, target, accumulator)
+        }
+    }
+
+    // Function to build a tree
+    private fun buildTree(rootValue: Long, values: List<String>): TreeNode {
+
+        fun createLevel(currentNode: TreeNode, currentLevel: Int) {
+            val nextLevel = currentLevel + 1
+            if (nextLevel == values.size) {
+                return
+            }
+
+            val toLong = values[nextLevel].toLong()
+
+            val additionNode = TreeNode(value = currentNode.value + toLong)
+            currentNode.children.add(additionNode)
+            createLevel(additionNode, nextLevel)
+
+            val multiplyNode = TreeNode(value = currentNode.value * toLong)
+            currentNode.children.add(multiplyNode)
+            createLevel(multiplyNode, nextLevel)
+        }
+
+        val root = TreeNode(rootValue)
+        createLevel(root, 1)
+        return root
+    }
+}
